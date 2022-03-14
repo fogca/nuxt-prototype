6970d2f9b2bc1e384a7bb89a461f3f06768db9da
one-tokyo <info@one.tokyo.jp>
2022-03-14 15:09:05 +0900

    0314_1510

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,10 +12,17 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/icons/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', sizes:'32x32', href: '/icons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/x-icon', sizes:'16x16', href: '/icons/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', type: 'image/x-icon', sizes:'180x180', href: '/icons/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/icons/site.webmanifest' },
+      { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color:'#5bbad5' },
     ]
   },
 
